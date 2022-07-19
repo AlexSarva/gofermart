5739e6c19d66290cbc75d679936758fae39cc9a3
Alex Sarva <alexsarva@yandex.ru>
2022-07-19 18:38:05 +0300

    fix: test server v0.4

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/izumin5210/httplogger v1.0.0 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 	github.com/lib/pq v1.10.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- loyality/client.go
+++ loyality/client.go
@@ -9,7 +9,6 @@ import (
 	"gopkg.in/h2non/gentleman-retry.v2"
 	"gopkg.in/h2non/gentleman.v2"
 	"gopkg.in/h2non/gentleman.v2/plugins/timeout"
-	"gopkg.in/h2non/gentleman.v2/plugins/url"
 	"log"
 	"net/http"
 	"time"
@@ -25,9 +24,10 @@ type ProcessingClient struct {
 func NewProcessingClient(serviceAddress, basicURL string) *ProcessingClient {
 	log.Println("LoyalityServer: ", serviceAddress+basicURL)
 	cli := gentleman.New()
+	//cli.Use(logger.New(os.Stdout))
 	cli.Use(timeout.Request(60 * time.Second))
 	cli.Use(retry.New(retrier.New(retrier.ExponentialBackoff(5, 100*time.Millisecond), nil)))
-	cli.Use(url.BaseURL(serviceAddress + basicURL))
+	cli.URL(serviceAddress + basicURL)
 	return &ProcessingClient{
 		Client: cli,
 	}
@@ -35,7 +35,8 @@ func NewProcessingClient(serviceAddress, basicURL string) *ProcessingClient {
 
 func (pc *ProcessingClient) GetOrder(orderNum string) (models.ProcessingOrder, error) {
 	req := pc.Client.Request()
-	req.Path(fmt.Sprintf("/%s", orderNum))
+	req.Method("GET")
+	req.AddPath(fmt.Sprintf("/%s", orderNum))
 	res, err := req.Send()
 	var order models.ProcessingOrder
 	if err != nil {
